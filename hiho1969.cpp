be0bd8f85baa8f9d70335cae1d0dab67beafd4ea
qanny <349117102@qq.com>
2019-06-11 20:36:18 +0800

    20190611

--- hiho1969.cpp
+++ hiho1969.cpp
@@ -6,72 +6,102 @@
 #define MAXN 1000002
 
 int nNum[MAX];
-bool bNum[MAXN];
+int bNum[MAXN];
+
+#define DEBUG false
+
+int nTotalNum;
+
+void output()
+{
+	if (DEBUG)
+     {
+         for (int i = 0; i < nTotalNum; i++)
+             std::cout << " " <<  nNum[i];
+     }
+}
 
 void qSort(int nStartPos, int nEndPos)
 {
+
 	if (nStartPos >= nEndPos) return;
 
     int start = nStartPos, end = nEndPos;
 	int curPos = start;
 
+
+
 	bool flag = true;
 	while(flag)
 	{
 		flag = false;
-	while(start < curPos && nNum[start] < nNum[curPos]) start++;
-	if (start < curPos)
-	{
-		int tmp = nNum[start];
-		nNum[start] = nNum[curPos];
-		nNum[curPos] = tmp;
-		curPos = start;
-		flag = true;
-	}
-	while(end > curPos && nNum[end] > nNum[curPos]) end--;
-	if (end > curPos)
-	{
-		int tmp = nNum[end];
-		nNum[end] = nNum[curPos];
-		nNum[curPos] = tmp;
-		curPos = end;
-		flag = true;
+		while(start < curPos && nNum[start] <= nNum[curPos]) start++;
+		if (start < curPos)
+		{
+			int tmp = nNum[start];
+			nNum[start] = nNum[curPos];
+			nNum[curPos] = tmp;
+			curPos = start;
+			flag = true;
 		}
+		while(end > curPos && nNum[end] >= nNum[curPos]) end--;
+		if (end > curPos)
+		{
+			int tmp = nNum[end];
+			nNum[end] = nNum[curPos];
+			nNum[curPos] = tmp;
+			curPos = end;
+			flag = true;
+		}
+	
 	}
 
-	qSort(nStartPos, curPos);
-	qSort(curPos + 1, nEndPos);
+	if (nStartPos < curPos)
+		qSort(nStartPos, curPos);
+	if (curPos + 1 < nEndPos) 
+		qSort(curPos + 1, nEndPos);
 }	
 
-#define DEBUG true
+
 
 int main() {
-	int nTotalNum, nTmp;
+	int nTmp;
 
 	memset(nNum, 0, MAX);
+	memset(bNum, 0, MAXN);
 
 	std::cin >> nTotalNum;
-	for (int i = 0; i < nTotalNum; i++) {
+	int nRepeatNum = 0;
+	for (int i = 0; i < nTotalNum; i++)
+	{
 		std::cin >> nNum[i];
-		bNum[nTmp] = true;
+		bNum[nNum[i]]++;
+		if (bNum[nNum[i]] >= 3 && bNum[nNum[i]] > nRepeatNum)
+			nRepeatNum = bNum[nNum[i]];
+	}
+
+	if (nTotalNum < 2) {
+		std::cout << "0" << std::endl;
+		return 0;
 	}
 
 	qSort(0, nTotalNum - 1);
 
-	if (DEBUG)
-	{
-		for (int i = 0; i < nTotalNum; i++) 
-			std::cout << " " <<  nNum[i];
-	} 
-	
-	if (nTotalNum < 2) return 0;
 
-	int nMaxCount = 0;
+	int nMaxCount = nRepeatNum;
 	int nCurCount = 2;
+	int nCurStartPos = 0, nCurNextPos = 0;
 	for (int i = 0; i < nTotalNum; i++) 
 	{
+
 		for (int j = i + 1; j < nTotalNum; j++) 
 		{
+			if (nMaxCount != 0)
+			{
+				if (nNum[nCurStartPos] + (nMaxCount - 1) * (nNum[nCurNextPos] - nNum[nCurStartPos]) > nNum[nTotalNum - 1]) break;
+			}
+			if (nNum[i] - nNum[j] + nNum[i] >= 0 && bNum[nNum[i] - nNum[j] + nNum[i]]) continue; 
+
 			nCurCount = 2;
 			int nTarget = nNum[j] + nNum[j] - nNum[i];  
 
@@ -79,14 +109,19 @@ int main() {
 			while (1)
 			{
 				if (bNum[nTarget]) 	nCurCount++;
+				else break;
 				nTarget = nTarget + nNum[j] - nNum[i];	
 			}
 
-			if (nCurCount >= 3 && nCurCount > nMaxCount) nMaxCount = nCurCount;	
+			if (nCurCount >= 3 && nCurCount > nMaxCount) {
+				nMaxCount = nCurCount;	
+				nCurStartPos = i;
+				nCurNextPos = j;
+			}
 		}
 	}
 
-	std::cout << nMaxCount;
+	std::cout << nMaxCount << std::endl;
 
 	return 0;
 }
