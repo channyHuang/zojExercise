eae96e6036720d593d1aa701b5b45888987b49ec
qanny <349117102@qq.com>
2019-07-23 21:25:07 +0800

    20190723

---
+++ hiho1842.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+#define MAXN 1005
+
+ 
+
+int main() {
+	int numOfA, numOfB;
+	std::cin >> numOfA >> numOfB;
+	int oneOfA = numOfA, twoOfA = 0;
+	int oneOfB = numOfB, twoOfB = 0;
+	int blockOfA, blockOfB;
+	bool flag = false;
+	for (; oneOfA >= 2; oneOfA -= 2, twoOfA++) {
+		blockOfA = oneOfA + twoOfA;
+		if (blockOfA > numOfB + 1 || blockOfA < numOfB / 2 + numOfB % 2 - 1) continue;
+		for (oneOfB = numOfB, twoOfB = 0; oneOfB >= 2; oneOfB -= 2, twoOfB++) {
+			if (oneOfB + twoOfB <= blockOfA + 1 && oneOfB + twoOfB >= blockOfA - 1) break;
+		}
+		blockOfB = oneOfB + twoOfB;
+		if (blockOfB <= blockOfA + 1 && blockOfB >= blockOfA - 1) {
+			flag = true;
+			break;
+		}
+	}
+	std::cout << blockOfA << " " << blockOfB << " " <<  flag << std::endl;
+	bool lastChar = 1;//0-A, 1-B
+	if (flag) {
+		while(blockOfA || blockOfB) {
+				
+			if (blockOfA > blockOfB) {
+				if (oneOfA) {
+					std::cout << "A";
+					oneOfA--;
+				} else {
+					std::cout << "AA";
+					twoOfA--;
+				}
+				blockOfA--;
+				lastChar = 0;
+			} else if (blockOfB > blockOfA) {
+				if (oneOfB) {
+					std::cout << "B";
+					oneOfB--;
+				} else {
+					std::cout << "BB";
+					twoOfB--;
+				}
+				blockOfB--;
+				lastChar = 1;
+			} else {
+				if (oneOfA) {
+					std::cout << "A";
+					oneOfA--;
+				} else {
+					std::cout << "AA";
+					twoOfA--;
+				}
+				if (oneOfB) {
+					std::cout << "B";
+					oneOfB--;
+				} else {
+					std::cout << "BB";
+					twoOfB--;
+				}
+				blockOfA--;
+				blockOfB--;
+			}
+		}
+
+	}
+	else std::cout << "-1" << std::endl;
+	return 0;
+}
--- hiho1846_WA.cpp
+++ hiho1846_WA.cpp
@@ -5,13 +5,14 @@ int objs[8] = {0};
 
 int minCost;
 void search(int curObj, int curRes, bool flags[3]) {
+	if (curObj >= 7) return;
 	if (flags[0] && flags[1] && flags[2]) {
 		if (curRes < minCost) minCost = curRes;
 		return;
 	}
 	search(curObj + 1, curRes, flags);	
+	if (objs[curObj] == 0) return;
 
-	curRes += objs[curObj];
 	if (curObj < 3) flags[curObj] = true;
 	else if (curObj == 3) {
 		flags[0] = true;
@@ -27,7 +28,28 @@ void search(int curObj, int curRes, bool flags[3]) {
 		flags[1] = true;
 		flags[2] = true;
 	}
-	search(curObj + 1, curRes, flags);
+	search(curObj + 1, curRes + objs[curObj], flags);
+	if (curObj < 3) flags[curObj] = false;
+	else if (curObj == 3) {
+		flags[0] = false;
+		flags[1] = false;
+	} else if (curObj == 4) {
+		flags[0] = false;
+		flags[2] = false;
+	} else if (curObj == 5) {
+		flags[1] = false;
+		flags[2] = false;
+	} else {
+		flags[0] = false;
+		flags[1] = false;
+		flags[2] = false;
+	}
+
+}
+
+void output() {
+	for (int i = 0; i < 7; i++) std::cout << " " << objs[i];
+	std::cout << std::endl;
 }
 
 int main() {
@@ -40,19 +62,20 @@ int main() {
 		int len = 0;
 		while(tmpObj[len] != 0) len++;
 		if (len == 1) {
-			if (objs[tmpObj[0] - 'A'] > tmpNo) objs[tmpObj[0] - 'A'] = tmpNo;
+			if (objs[tmpObj[0] - 'A'] == 0 || objs[tmpObj[0] - 'A'] > tmpNo) objs[tmpObj[0] - 'A'] = tmpNo;
 		} else if (len == 3) {
-			if (objs[6] > tmpNo) objs[6] = tmpNo;
+			if (objs[6] == 0 || objs[6] > tmpNo) objs[6] = tmpNo;
 		} else {
 			if ('A' != tmpObj[0] && 'A' != tmpObj[1]) {
-				if (objs[5] > tmpNo) objs[5] = tmpNo;
+				if (objs[5] == 0 || objs[5] > tmpNo) objs[5] = tmpNo;
 			} else if ('B' != tmpObj[0] && 'B' != tmpObj[1]) {
-				if (objs[4] > tmpNo) objs[4] = tmpNo;	
+				if (objs[4] == 0 || objs[4] > tmpNo) objs[4] = tmpNo;	
 			} else {
-				if (objs[3] > tmpNo) objs[3] = tmpNo;
+				if (objs[3] == 0 || objs[3] > tmpNo) objs[3] = tmpNo;
 			}
 		}
 	}
+	//output();
 	if (objs[0] + objs[3] + objs[4] + objs[6] == 0) minCost = -1;
 	else if (objs[1] + objs[3] + objs[5] + objs[6] == 0) minCost = -1;
 	else if (objs[2] + objs[4] + objs[5] + objs[6] == 0) minCost = -1;
